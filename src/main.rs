0c34bce199c25a964eca1493cf9474a1770bba50
mau5atron <gabrielbetancourt96@outlook.com>
2022-09-13 00:28:56 -0400

    Setup Rocket dynamic Tera template

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 #[macro_use] extern crate rocket;
+extern crate rocket_dyn_templates;
+use rocket_dyn_templates::{Template, context};
+
+// #[get("/hello/world")]
+// fn index() -> &'static str {
+//     "Hello, world!"
+// }
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", context! {field: "some_value"})
 }
 
+// #[launch] infers the return type
 #[launch]
 fn rocket() -> _ {
     rocket::build()
     .mount("/", routes![index])
+    .attach(Template::fairing())
 }
